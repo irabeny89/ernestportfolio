22f882025fa0553f97b73dfcd7825d136d0a2392
Ernest Irabor <irabeny89@gmail.com>
2021-01-14 21:00:39 +0100

    🎨 Improve UX

--- src/components/DevImage.js
+++ src/components/DevImage.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 import React from 'react'
 
 const DevImage = ({imageProp}) => 
-  <div>
+  <>
     <Image src={imageProp.src} alt={imageProp.alt} width="180" height="300" />
-  </div>
+  </>
 
 export default DevImage
