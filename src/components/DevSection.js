7fa1bea98137b173c9130abe86d9d2cf2c3e4264
Ernest Irabor <irabeny89@gmail.com>
2021-01-15 02:40:39 +0100

    💄 🎨 Perfomance and style update

--- src/components/DevSection.js
+++ src/components/DevSection.js
@@ -3,11 +3,13 @@ import DevImage from './DevImage'
 import { FaTools } from 'react-icons/fa'
 import styles from '../../styles/DevSection.module.css'
 
-const DevSection = ({projects, type, tools}) => 
+const DevSection = ({ projects, type, tools }) =>
   <div className={styles.devSection}>
     <h3>{type}</h3>
-    <h4><FaTools /> Tools used: <span>{tools.join(', ')}</span></h4>
-    {projects.map(({id, image, description, website, tech}) => 
+    <h4>
+      <FaTools /> Tools used: <span>{tools.join(', ')}</span>
+    </h4>
+    {projects.map(({ id, image, description, website, tech }) =>
       <div key={id}>
         <h4>{description}</h4>
         <p>Tech: {tech}</p>
