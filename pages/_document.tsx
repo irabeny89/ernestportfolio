1fa1d00edff1ee8ca696618d049d320415b92ed1
Ernest Irabor <irabeny89@gmail.com>
2021-06-02 23:25:09 +0100

    main document initial commit

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, {
+  Html, Head, Main, NextScript
+} from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="description" content="Ernest portfolio website" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
