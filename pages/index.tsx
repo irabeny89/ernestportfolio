5af65947a06ef78ef15b57a0991ba1ee0f90f68f
Ernest Irabor <irabeny89@gmail.com>
2021-06-02 23:29:57 +0100

    home page initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const Home = () =>
+  <>
+    <Head>
+      <title>Ernest Portfolio</title>
+    </Head>
+    <div className="pt-5 mt-5">
+      Home
+    </div>
+  </>
+
+export default Home
