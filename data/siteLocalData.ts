1e178bf1eb841b4661aaeab676935e569405ed07
Ernest Irabor <irabeny89@gmail.com>
2021-06-02 21:37:27 +0100

    site data initial commit

---
+++ data/siteLocalData.ts
@@ -0,0 +1,79 @@
+const siteLocalData = {
+  avatar: {
+    width: "32",
+    height: "32",
+    src: "",
+    alt: "avatar",
+  },
+  intro: "As a software developer I get things done using: ",
+  tools: [
+    "JavaScript",
+    "TypeScript",
+    "Nodejs",
+    "Expressjs",
+    "Nextjs",
+    "Reactjs",
+    "MongoDB",
+    "MySQL",
+    "GraphQL",
+    "PostgreSQL",
+  ],
+  projects: [
+    {
+      image: {
+        width: "32",
+        height: "32",
+        src: "",
+        alt: "project",
+      },
+      title: "Happen",
+      summary: "Social media app with follow/unfollow, news feeds, posts etc.",
+      link: {
+        live: "http://happen.herokuapp.com",
+        github: "https://irabeny89/happen",
+      },
+    },
+    {
+      image: {
+        width: "32",
+        height: "32",
+        src: "",
+        alt: "project",
+      },
+      title: "E Note",
+      summary: "Browser Local storage note app with full CRUD functions.",
+      link: {
+        live: "https://enote-beta.vercel.app/",
+        github: "https://irabeny89/enote",
+      },
+    },
+    {
+      image: {
+        width: "32",
+        height: "32",
+        src: "",
+        alt: "project",
+      },
+      title: "Server Note",
+      summary: "Server note app with full API CRUD functions.",
+      link: {
+        live: "https://servernoteapp.herokuapp.com/",
+        github: "https://irabeny89/servernote",
+      },
+    },
+  ],
+  contacts: [
+    { type: "Phone & Whatsapp", line: "09020951797" },
+    { type: "Email", line: "irabeny89@gmail.com" },
+    {
+      type: "Linkedin",
+      line: "https://linkedin.com/in/ernest-irabor-085486183",
+    },
+    { type: "Github", line: "https://github.com/irabeny89" },
+  ],
+  cv: {
+    url: "https://drive.google.com/file/d/1a6XM-o_fZBNH1Eq17aOm9ySb5ygGabT-/view?usp=sharing",
+  },
+};
+
+export default siteLocalData;
