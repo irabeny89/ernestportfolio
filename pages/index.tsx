7d36f67091a940a0ae090762c144c07949aaf13f
Ernest Irabor <irabeny89@gmail.com>
2021-06-04 05:43:27 +0100

    responsive fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Intro from "../components/Intro";
+import Projects from "../components/Projects";
 
-const Home = () =>
-  <>
-    <Head>
-      <title>Ernest Portfolio</title>
-    </Head>
-    <div className="pt-5 mt-5">
-      Home
-    </div>
-  </>
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Ernest Portfolio</title>
+      </Head>
+      <div className="pt-5 mt-5" style={{minWidth: "450px"}}>
+        <Intro />
+        <Projects />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
