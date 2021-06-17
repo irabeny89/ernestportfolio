d9e1b939f400e206830a1bfdc79055afd250ecfc
Ernest Irabor <irabeny89@gmail.com>
2021-06-18 00:52:54 +0100

    refactor

--- data/siteLocalData.ts
+++ data/siteLocalData.ts
@@ -6,7 +6,7 @@ const siteLocalData = {
     src: "/Ernest cartoon.jpg",
     alt: "avatar",
   },
-  intro: "I am a software developer, I build fullstack apps using:",
+  intro: "I am a software developer, I build interactive web apps using modern tools.",
   tools: [
     "JavaScript",
     "TypeScript",
@@ -27,10 +27,24 @@ const siteLocalData = {
         alt: "project",
       },
       title: "Happen Social",
-      summary: "Social media app with follow/unfollow, news feeds, posts etc.",
+      summary: "Social media app.",
       link: {
         live: "http://happen.herokuapp.com",
         github: "https://github.com/irabeny89/mern-social-happen",
+      },
+    },
+    {
+      image: {
+        width: "320",
+        height: "250",
+        src: "/employee-api.png",
+        alt: "project",
+      },
+      title: "Employee API",
+      summary: "Employee management API.",
+      link: {
+        live: "https://crudapi-employee.herokuapp.com/",
+        github: "https://github.com/irabeny89/employee-api",
       },
     },
     {
@@ -41,7 +55,7 @@ const siteLocalData = {
         alt: "project",
       },
       title: "E Note",
-      summary: "Browser Local storage note app with full CRUD functions.",
+      summary: "Note taking app.",
       link: {
         live: "https://enote-beta.vercel.app/",
         github: "https://github.com/irabeny89/enote",
@@ -51,14 +65,28 @@ const siteLocalData = {
       image: {
         width: "320",
         height: "250",
-        src: "/express-notesapp.png",
+        src: "/superbase.png",
         alt: "project",
       },
-      title: "Server Note",
-      summary: "Server note app with full API CRUD functions.",
+      title: "Superbase",
+      summary: "A business web app.",
       link: {
-        live: "https://servernoteapp.herokuapp.com/",
-        github: "https://github.com/irabeny89/servernotes",
+        live: "https://superbase.vercel.app/",
+        github: "https://github.com/irabeny89/superbase",
+      },
+    },
+    {
+      image: {
+        width: "320",
+        height: "250",
+        src: "/dcpicker.png",
+        alt: "project",
+      },
+      title: "DC Picker",
+      summary: "A guessing game.",
+      link: {
+        live: "https://dcpicker.vercel.app/",
+        github: "https://github.com/irabeny89/dcpicker",
       },
     },
   ],
