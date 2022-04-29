ad3ec7baf2fe9b7dad13fbd0f1b566a38a23d4a6
Ernest Irabor <irabeny89@gmail.com>
2022-04-29 04:32:45 +0100

    add header component tests

---
+++ __tests__/components/Header.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Header from "components/Header";
+import siteLocalData from "data/siteLocalData";
+
+const {
+    author,
+    contacts,
+    cv: { url },
+  } = siteLocalData,
+  phoneWhatsApp = contacts.find(({ type }) => type.includes("Phone"));
+
+describe("Header component", () => {
+  beforeEach(() => render(<Header />).debug());
+  afterEach(cleanup);
+
+  it("renders my full name.", () => {
+    const header = screen.getByRole("heading");
+
+    expect(header).toHaveTextContent(author);
+  });
+
+  it("renders my contacts with links.", async () => {
+    const user = userEvent.setup(),
+      contactsDropdown = screen.getByRole("button", {
+        name: /contacts/i,
+      });
+
+    await user.click(contactsDropdown);
+
+    contacts.forEach(async ({ type, line }) =>
+      type.includes("Phone")
+        ? expect(await screen.findByText(type)).toHaveAttribute(
+            "href",
+            `tel:${line}`
+          )
+        : type.includes("Email")
+        ? expect(await screen.findByText(type)).toHaveAttribute(
+            "href",
+            `mailto:${line}`
+          )
+        : expect(await screen.findByText(type)).toHaveAttribute("href", line)
+    );
+  });
+
+  it("has a link to my CV.", () => {
+    const user = userEvent.setup(),
+      cvLink = screen.getByRole("link");
+
+    expect(cvLink).toHaveAttribute("href", url);
+  });
+
+  it("it matches snapshot.", () => {
+    expect(screen.getByRole("navigation")).toMatchSnapshot();
+  });
+});
