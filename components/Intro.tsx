784d531731b6625439a5649feb3f06026b0c79d2
Ernest Irabor <irabeny89@gmail.com>
2022-04-30 19:54:22 +0100

    refactor code

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -11,66 +11,53 @@ import { GrReactjs, GrGraphQl } from "react-icons/gr";
 
 const Intro = () => {
   const { tools, intro } = siteLocalData;
+
   return (
-    <div>
+    <div data-testid="intro">
       <p className="display-4 text-center mb-5">{intro}</p>
-      <div className="d-flex justify-content-around flex-wrap">
-        {tools.map((tool, i) => {
-          if (tool === "JavaScript")
-            return (
-              <div key={i}>
-                <SiJavascript color="#cc0" /> {tool}
-              </div>
-            );
-          if (tool === "TypeScript")
-            return (
-              <div key={i}>
-                <SiTypescript color="blue" /> {tool}
-              </div>
-            );
-          if (tool === "Nodejs")
-            return (
-              <div key={i}>
-                <SiNodedotjs color="green" /> {tool}
-              </div>
-            );
-          if (tool === "Nextjs")
-            return (
-              <div key={i}>
-                <SiNextdotjs color="black" /> {tool}
-              </div>
-            );
-          if (tool === "Reactjs")
-            return (
-              <div key={i}>
-                <GrReactjs color="cyan" /> {tool}
-              </div>
-            );
-          if (tool === "MongoDB")
-            return (
-              <div key={i}>
-                <DiMongodb color="green" /> {tool}
-              </div>
-            );
-          if (tool === "MySQL")
-            return (
-              <div key={i}>
-                <DiMysql color="blue" /> {tool}
-              </div>
-            );
-          if (tool === "GraphQL")
-            return (
-              <div key={i}>
-                <GrGraphQl color="crimson" /> {tool}
-              </div>
-            );
-          if (tool === "PostgreSQL")
-            return (
-              <div key={i}>
-                <SiPostgresql color="#00d" /> {tool}
-              </div>
-            );
-        })}
+      <div
+        className="d-flex justify-content-around flex-wrap"
+        data-testid="tools"
+      >
+        {tools.map((tool, i) =>
+          tool === "JavaScript" ? (
+            <div key={i}>
+              <SiJavascript color="#cc0" /> {tool}
+            </div>
+          ) : tool === "TypeScript" ? (
+            <div key={i}>
+              <SiTypescript color="blue" /> {tool}
+            </div>
+          ) : tool === "Nodejs" ? (
+            <div key={i}>
+              <SiNodedotjs color="green" /> {tool}
+            </div>
+          ) : tool === "Nextjs" ? (
+            <div key={i}>
+              <SiNextdotjs color="black" /> {tool}
+            </div>
+          ) : tool === "Reactjs" ? (
+            <div key={i}>
+              <GrReactjs color="cyan" /> {tool}
+            </div>
+          ) : tool === "MongoDB" ? (
+            <div key={i}>
+              <DiMongodb color="green" /> {tool}
+            </div>
+          ) : tool === "MySQL" ? (
+            <div key={i}>
+              <DiMysql color="blue" /> {tool}
+            </div>
+          ) : tool === "GraphQL" ? (
+            <div key={i}>
+              <GrGraphQl color="crimson" /> {tool}
+            </div>
+          ) : tool === "PostgreSQL" ? (
+            <div key={i}>
+              <SiPostgresql color="#00d" /> {tool}
+            </div>
+          ) : null
+        )}
       </div>
     </div>
   );
