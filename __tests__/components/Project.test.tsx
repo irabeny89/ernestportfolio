ee128d7de17eb4b7a934f6a521a29ca5ff217eb1
Ernest Irabor <irabeny89@gmail.com>
2022-05-01 01:34:02 +0100

    add project component test

---
+++ __tests__/components/Project.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import Project from "components/Projects";
+import siteLocalData from "data/siteLocalData";
+
+const { projects } = siteLocalData;
+
+describe("Project component", () => {
+  beforeEach(() => render(<Project />));
+
+  it("list my projects with details and links.", () => {
+    const projectList = screen.getAllByTestId("project");
+
+    projects.forEach(({ title, summary, link: { live, github } }, i) => {
+      expect(projectList[i].children[0].textContent).toBe(title);
+      expect(projectList[i].children[1].textContent).toBe(summary);
+      expect(projectList[i].children[3].children[0]).toHaveAttribute(
+        "href",
+        live
+      );
+      expect(projectList[i].children[3].children[1]).toHaveAttribute(
+        "href",
+        github
+      );
+    });
+  });
+
+  it("matches snapshot", () => {
+    expect(screen.getAllByTestId("project")).toMatchSnapshot();
+  });
+});
