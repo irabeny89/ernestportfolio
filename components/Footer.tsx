8cdcf585c5ca19d163c25a1eb50a56261c77566e
Irabeny <irabeny89@gmail.com>
2023-07-30 10:41:42 +0100

    refactor: update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,38 +1,12 @@
-import { FaWhatsapp, FaLinkedin, FaGithub } from "react-icons/fa";
-import { CgMail } from "react-icons/cg";
 import siteLocalData from "../siteLocalData";
 
 const Footer = () => {
-  const { contacts, copyWriteYear } = siteLocalData;
-  return (
-    <div
-      className="d-flex justify-content-between bg-light text-primary py-2"
-      data-testid="footer"
-    >
-      <h3>&copy; {copyWriteYear}</h3>
-      <div className="d-flex justify-content-around w-25">
-        {contacts.map(({ type, line }, i) =>
-          type.includes("Whatsapp") ? (
-            <a href={`tel:${line}`} key={i}>
-              <FaWhatsapp size="40" />
-            </a>
-          ) : type.includes("Email") ? (
-            <a href={`mailto:${line}`} key={i}>
-              <CgMail size="40" />
-            </a>
-          ) : type.includes("Linkedin") ? (
-            <a href={line} key={i}>
-              <FaLinkedin size="40" />
-            </a>
-          ) : type.includes("Github") ? (
-            <a href={line} key={i}>
-              <FaGithub size="40" />
-            </a>
-          ) : null
-        )}
-      </div>
-    </div>
-  );
+	const { copyWriteYear, title } = siteLocalData;
+	return (
+		<footer className="text-center" data-testid="footer">
+			<h3>{title} &copy; {copyWriteYear}</h3>
+		</footer>
+	);
 };
 
 export default Footer;
